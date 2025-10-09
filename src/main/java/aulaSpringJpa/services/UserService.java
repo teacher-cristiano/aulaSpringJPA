0406b8d294cdbbdf6849c6864289da449ddf938d
Cristiano <cristianonetto@gmail.com>
2025-10-09 10:30:54 -0300

    correção da conexão do bd

--- src/main/java/aulaSpringJpa/services/UserService.java
+++ src/main/java/aulaSpringJpa/services/UserService.java
@@ -3,9 +3,11 @@ package aulaSpringJpa.services;
 import aulaSpringJpa.entities.User;
 import aulaSpringJpa.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService {
 
     @Autowired
