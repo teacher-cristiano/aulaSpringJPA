48609ea9accf8156a68ddc874914e923c39c6fe1
Cristiano <cristianonetto@gmail.com>
2025-10-22 15:30:15 -0300

    ajuste User

--- src/main/java/aulaSpringJpa/controllers/UserController.java
+++ src/main/java/aulaSpringJpa/controllers/UserController.java
@@ -32,9 +32,28 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj){
         obj = userService.insert(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-                .buildAndExpand(obj.getId()).toUri();
-        return ResponseEntity.created(uri).body(obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
+//    @PostMapping
+//    public ResponseEntity<User> insert(@RequestBody User obj){
+//        obj = userService.insert(obj);
+//        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+//                .buildAndExpand(obj.getId()).toUri();
+//        return ResponseEntity.created(uri).body(obj);
+//        //return ResponseEntity.ok().body(obj);
+//    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj = userService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
